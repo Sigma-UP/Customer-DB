898e445014a3b020637dad9127857362b19fab9a
Sigma-UP <62567341+Sigma-UP@users.noreply.github.com>
2021-06-14 23:50:06 +0300

    add select query for ALL REQUIRED data

---
+++ queries/table/selectAllRequiredData.sql
@@ -0,0 +1,8 @@
+SELECT Customers.CustomerID, Customers.LastName,
+	 Notes.Note, TotalPurchasesAmount,
+	 Addresses.AddressLine, AddressType,
+	 Addresses.City, Addresses.PostalCode,
+	 Addresses.[State], Addresses.Country
+FROM [dbo].[Customers], [dbo].[Addresses], [dbo].[Notes]
+WHERE Addresses.CustomerID = Customers.CustomerID AND Notes.CustomerID = Customers.CustomerID
+ORDER BY Customers.CustomerID;
