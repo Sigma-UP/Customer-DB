f11b232bcb66dd0c4c9f0b89d1febfe02fc99c57
Sigma-UP <62567341+Sigma-UP@users.noreply.github.com>
2021-06-15 02:05:37 +0300

    add data insertion file (WIP)

---
+++ queries/data.sql
@@ -0,0 +1,26 @@
+USE [CustomerLib_Bezslyozniy]
+
+INSERT INTO [dbo].[Customers] (FirstName, LastName, PhoneNumber, Email, TotalPurchasesAmount)
+VALUES ('Anton', 'Krund', '+34122345', 'amail@ha.com', 0);
+GO
+INSERT INTO [dbo].[Customers] (FirstName, LastName, PhoneNumber, Email, TotalPurchasesAmount)
+VALUES ('Sergei', 'Krund', '+34532323', 'smail@ha.ru', 194);
+GO
+INSERT INTO [dbo].[Customers] (FirstName, LastName, PhoneNumber, Email, TotalPurchasesAmount)
+VALUES ('Ivan', 'Tarasov', '+34345645', 'imail@ha.com', 1354.4);
+GO
+INSERT INTO [dbo].[Customers] (FirstName, LastName, PhoneNumber, Email, TotalPurchasesAmount)
+VALUES ('34', 'Sergeev', '+345432', 'smail@ha.ru', 0);
+GO
+INSERT INTO [dbo].[Customers] (FirstName, LastName, PhoneNumber, Email, TotalPurchasesAmount)
+VALUES ('Aleksey', '34', '+345432', 'smail@ha.ru', 0);
+GO
+INSERT INTO [dbo].[Customers] (FirstName, LastName, PhoneNumber, Email, TotalPurchasesAmount)
+VALUES ('Aleksey', 'Sergeev', '+3ee45432', 'smail@ha.ru', 0);
+GO
+INSERT INTO [dbo].[Customers] (FirstName, LastName, PhoneNumber, Email, TotalPurchasesAmount)
+VALUES ('Aleksey', 'Sergeev', '+345432', 'smailha.ru', 0);
+GO
+INSERT INTO [dbo].[Customers] (FirstName, LastName, PhoneNumber, Email, TotalPurchasesAmount)
+VALUES ('Aleksey', 'Sergeev', '+345432', 'smail@ha.ru', -12);
+GO
